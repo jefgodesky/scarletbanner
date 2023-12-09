67ab418fa034e1cac1b4c4f25d4e925cc8f0382f
Jason Godesky <jefgodesky@gmail.com>
2023-12-09 13:13:18 -0500

    TestUserViewSet: Refactor tests

--- scarletbanner/users/tests/test_drf_views.py
+++ scarletbanner/users/tests/test_drf_views.py
@@ -1,11 +1,10 @@
 import pytest
 from django.contrib.auth.models import AnonymousUser
 from django.db import transaction
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.parsers import JSONParser
 from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.test import APIRequestFactory, force_authenticate
+from rest_framework.test import APIRequestFactory
 
 from scarletbanner.users.api.serializers import UserCreateSerializer, UserPublicSerializer, UserSerializer
 from scarletbanner.users.api.views import UserViewSet
@@ -19,101 +18,94 @@ class TestUserViewSet:
         return APIRequestFactory()
 
     @staticmethod
-    def get_view(user: User | AnonymousUser, url: str, api_rf: APIRequestFactory):
+    def request(
+        url: str,
+        action: str,
+        method: str,
+        subj: User | AnonymousUser,
+        api_rf: APIRequestFactory,
+        obj: User = None,
+        data: dict = None,
+    ):
         view = UserViewSet()
-        request = api_rf.get(url)
-        request.user = user
-        view.action = "retrieve"
-        view.request = request
+        req_method = getattr(api_rf, method)
+        dj_request = req_method(url, data, format="json") if data is not None else req_method(url)
+        view.action = action
+        view.request = Request(dj_request, parsers=[JSONParser()])
+        view.request.user = subj
+        view.kwargs = {"username": obj.username} if obj is not None else {}
+        view.format_kwarg = None
+        view.request.url_kwargs = view.kwargs
         return view
 
-    @staticmethod
-    def post_data(data: dict, action: str, api_rf: APIRequestFactory):
-        django_request = api_rf.post("/fake-url/", data, format="json")
-        drf_request = Request(django_request, parsers=[JSONParser()])
-        view = UserViewSet()
-        view.action = action
-        view.request = drf_request
-        view.kwargs = {}
-        view.format_kwarg = None
+    def get_serializer(self, action: str, subj: User | AnonymousUser, api_rf: APIRequestFactory, obj: User = None):
+        url = "/fake-url/" if obj is None else f"/fake-url/{obj.username}/"
+        view = self.request(url, action, "get", subj, api_rf, obj)
+        return view.get_serializer_class()
 
-        try:
-            if action == "create":
-                return view.create(drf_request)
-        except ValidationError as ve:
-            return Response(ve.detail, status=ve.status_code)
+    def create(self, data: dict, api_rf: APIRequestFactory):
+        view = self.request("/fake-url/", "create", "post", AnonymousUser(), api_rf, None, data)
+        return view.create(view.request)
 
-    @staticmethod
-    def delete(username: str, user: User | AnonymousUser, api_rf: APIRequestFactory):
-        view = UserViewSet.as_view({"delete": "destroy"})
-        request = api_rf.delete(f"/fake-url/{username}/")
-        if not user.is_anonymous:
-            force_authenticate(request, user=user)
-        response = view(request, username=username)
-        return response
+    def destroy(self, subj: User | AnonymousUser, obj: User, api_rf: APIRequestFactory):
+        url = f"/fake-url/{obj.username}/"
+        view = self.request(url, "destroy", "delete", subj, api_rf, obj)
+        return view.destroy(view.request)
 
     def test_get_queryset(self, user: User, api_rf: APIRequestFactory):
-        view = self.get_view(user, "/fake-url/", api_rf)
+        view = self.request("/fake-url/", "retrieve", "get", AnonymousUser(), api_rf)
         assert user in view.get_queryset()
 
     def test_get_serializer_create(self, api_rf: APIRequestFactory):
-        view = self.get_view(AnonymousUser(), "/fake-url/", api_rf)
-        view.action = "create"
-        assert view.get_serializer_class() is UserCreateSerializer
+        serializer = self.get_serializer("create", AnonymousUser(), api_rf)
+        assert serializer is UserCreateSerializer
 
     def test_get_serializer_anon(self, user: User, api_rf: APIRequestFactory):
-        view = self.get_view(AnonymousUser(), f"/fake-url/{user.username}/", api_rf)
-        view.request.url_kwargs = {"username": user.username}
-        assert view.get_serializer_class() is UserPublicSerializer
+        serializer = self.get_serializer("retrieve", AnonymousUser(), api_rf, user)
+        assert serializer is UserPublicSerializer
 
     def test_get_serializer_self(self, user: User, api_rf: APIRequestFactory):
-        view = self.get_view(user, f"/fake-url/{user.username}/", api_rf)
-        view.request.url_kwargs = {"username": user.username}
-        assert view.get_serializer_class() is UserSerializer
+        serializer = self.get_serializer("retrieve", user, api_rf, user)
+        assert serializer is UserSerializer
 
     def test_get_serializer_other(self, user: User, api_rf: APIRequestFactory):
         other = UserFactory()
-        view = self.get_view(other, f"/fake-url/{user.username}/", api_rf)
-        view.request.url_kwargs = {"username": user.username}
-        assert view.get_serializer_class() is UserPublicSerializer
+        serializer = self.get_serializer("retrieve", other, api_rf, user)
+        assert serializer is UserPublicSerializer
 
     def test_get_serializer_staff(self, user: User, api_rf: APIRequestFactory):
         staff = UserFactory(is_staff=True)
-        view = self.get_view(staff, f"/fake-url/{user.username}/", api_rf)
-        view.request.url_kwargs = {"username": user.username}
-        assert view.get_serializer_class() is UserSerializer
+        serializer = self.get_serializer("retrieve", staff, api_rf, user)
+        assert serializer is UserSerializer
 
     @pytest.mark.django_db
     def test_create_success(self, api_rf: APIRequestFactory):
         data = {"username": "tester", "password": "testpassword123", "email": "tester@testing.com"}
-        response = self.post_data(data, "create", api_rf)
+        response = self.create(data, api_rf)
         assert response.status_code == 201
         assert User.objects.filter(username=data["username"]).exists()
 
     @pytest.mark.django_db
     def test_create_invalid_data(self, api_rf: APIRequestFactory):
         data = {"username": "", "email": "nope"}
-        response = self.post_data(data, "create", api_rf)
-        assert response.status_code == 400
-        assert not User.objects.filter(username=data["username"]).exists()
-
-    @pytest.mark.django_db
-    def test_destroy_not_found(self, user: User, api_rf: APIRequestFactory):
-        response = self.delete("not-a-user", user, api_rf)
-        assert response.status_code == 404
+        with pytest.raises(ValidationError):
+            response = self.create(data, api_rf)
+            assert response.status_code == 400
+            assert not User.objects.filter(username=data["username"]).exists()
 
     @pytest.mark.django_db
     def test_destroy_anon(self, user: User, api_rf: APIRequestFactory):
         with transaction.atomic():
-            response = self.delete(user.username, AnonymousUser(), api_rf)
-        user.refresh_from_db()
-        assert response.status_code == 401
-        assert user.is_active is True
+            with pytest.raises(PermissionDenied):
+                response = self.destroy(AnonymousUser(), user, api_rf)
+                user.refresh_from_db()
+                assert response.status_code == 401
+                assert user.is_active is True
 
     @pytest.mark.django_db
     def test_destroy_self(self, user: User, api_rf: APIRequestFactory):
         with transaction.atomic():
-            response = self.delete(user.username, user, api_rf)
+            response = self.destroy(user, user, api_rf)
         user.refresh_from_db()
         assert response.status_code == 200
         assert user.is_active is False
@@ -121,29 +113,25 @@ class TestUserViewSet:
     @pytest.mark.django_db
     def test_destroy_other(self, user: User, api_rf: APIRequestFactory):
         with transaction.atomic():
-            other = UserFactory()
-            response = self.delete(user.username, other, api_rf)
-        user.refresh_from_db()
-        assert response.status_code == 403
-        assert user.is_active is True
+            with pytest.raises(PermissionDenied):
+                other = UserFactory()
+                response = self.destroy(other, user, api_rf)
+                user.refresh_from_db()
+                assert response.status_code == 403
+                assert user.is_active is True
 
     @pytest.mark.django_db
     def test_destroy_staff(self, user: User, api_rf: APIRequestFactory):
         with transaction.atomic():
             staff = UserFactory(is_staff=True)
-            response = self.delete(user.username, staff, api_rf)
+            response = self.destroy(staff, user, api_rf)
         user.refresh_from_db()
         assert response.status_code == 200
         assert user.is_active is False
 
     def test_me(self, user: User, api_rf: APIRequestFactory):
-        view = UserViewSet()
-        request = api_rf.get("/fake-url/")
-        request.user = user
-
-        view.request = request
-
-        response = view.me(request)  # type: ignore
+        view = self.request("/fake-url/", "me", "get", user, api_rf)
+        response = view.me(view.request)
 
         assert response.data == {
             "username": user.username,
