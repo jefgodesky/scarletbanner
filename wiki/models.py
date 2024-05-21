025d54184e45e50e8885f5f7c2636aa1c6f0559e
Jason Godesky <jefgodesky@gmail.com>
2024-05-21 18:53:52 -0400

    wiki: Type methods

--- wiki/models.py
+++ wiki/models.py
@@ -6,37 +6,37 @@ from scarletbanner.users.models import User
 
 class WikiPage(models.Model):
     @property
-    def latest(self):
+    def latest(self) -> "Revision":
         return self.revisions.order_by('-timestamp', '-id').first()
 
     @property
-    def original(self):
+    def original(self) -> "Revision":
         return self.revisions.order_by('timestamp', 'id').first()
 
     @property
-    def title(self):
+    def title(self) -> str:
         return self.latest.title
 
     @property
-    def updated(self):
+    def updated(self) -> "datetime":
         return self.latest.timestamp
 
     @property
-    def created(self):
+    def created(self) -> "datetime":
         return self.original.timestamp
 
     @property
     def created_by(self) -> User:
         return self.original.editor
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.latest.title
 
-    def update(self, title, editor):
+    def update(self, title, editor) -> None:
         Revision.objects.create(title=title, page=self, editor=editor)
 
     @classmethod
-    def create(cls, title, editor):
+    def create(cls, title, editor) -> "WikiPage":
         page = cls.objects.create()
         Revision.objects.create(page=page, title=title, editor=editor)
         return page
@@ -48,5 +48,5 @@ class Revision(models.Model):
     editor = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="revisions", on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
