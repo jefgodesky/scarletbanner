5715ffe112c15bb432a82fced8733254e264f960
Jason Godesky <jefgodesky@gmail.com>
2024-06-02 16:15:00 -0400

    wiki: Add custom admin for secrets

--- scarletbanner/wiki/admin.py
+++ scarletbanner/wiki/admin.py
@@ -1,55 +1,108 @@
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import reverse
-from django.utils.html import format_html
-from scarletbanner.wiki.models import Character, OwnedPage, Page
+
+from scarletbanner.wiki.models import Character, OwnedPage, Page, Secret, SecretCategory
 
 
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
     fieldsets = (
-        (None, {
-            "fields": ("title", "body")
-        }),
-        ("Organization", {
-            "fields": ("parent", "slug"),
-        }),
-        ("Permissions", {
-            "fields": ("read", "write"),
-        }),
+        (None, {"fields": ("title", "body")}),
+        (
+            "Organization",
+            {
+                "fields": ("parent", "slug"),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": ("read", "write"),
+            },
+        ),
     )
 
 
 @admin.register(OwnedPage)
 class OwnedPageAdmin(admin.ModelAdmin):
     fieldsets = (
-        (None, {
-            "fields": ("title", "body")
-        }),
-        ("Ownership", {
-            "fields": ("owner",),
-        }),
-        ("Organization", {
-            "fields": ("parent", "slug"),
-        }),
-        ("Permissions", {
-            "fields": ("read", "write"),
-        }),
+        (None, {"fields": ("title", "body")}),
+        (
+            "Ownership",
+            {
+                "fields": ("owner",),
+            },
+        ),
+        (
+            "Organization",
+            {
+                "fields": ("parent", "slug"),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": ("read", "write"),
+            },
+        ),
     )
 
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
     fieldsets = (
-        (None, {
-            "fields": ("title", "body")
-        }),
-        ("Ownership", {
-            "fields": ("owner",),
-        }),
-        ("Organization", {
-            "fields": ("parent", "slug"),
-        }),
-        ("Permissions", {
-            "fields": ("read", "write"),
-        }),
+        (None, {"fields": ("title", "body")}),
+        (
+            "Ownership",
+            {
+                "fields": ("owner",),
+            },
+        ),
+        (
+            "Organization",
+            {
+                "fields": ("parent", "slug"),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": ("read", "write"),
+            },
+        ),
     )
+
+
+@admin.register(Secret)
+class SecretAdmin(admin.ModelAdmin):
+    change_list_template = "admin/tree.html"
+
+    def changelist_view(self, request, extra_context=None):
+        root_categories = SecretCategory.objects.filter(parent=None).prefetch_related("children", "secrets")
+        context = dict(
+            self.admin_site.each_context(request),
+            title="Secrets",
+            categories=root_categories,
+            add_category_url=reverse("admin:wiki_secretcategory_add"),
+            add_item_url=reverse("admin:wiki_secret_add"),
+        )
+        return render(request, "admin/tree.html", context)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if "categories" in request.GET:
+            form.base_fields["categories"].initial = [request.GET["categories"]]
+        return form
+
+
+@admin.register(SecretCategory)
+class SecretCategoryAdmin(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        return {}
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if "parent" in request.GET:
+            form.base_fields["parent"].initial = request.GET["parent"]
+        return form
