6388c64110f54cc7b43c2abfe9b7c72f196f05d2
Jason Godesky <jefgodesky@gmail.com>
2024-06-09 14:25:31 -0400

    GET /api/v1/wiki/ - Return human-readable permission data

--- scarletbanner/wiki/api/serializers.py
+++ scarletbanner/wiki/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from scarletbanner.wiki.enums import PermissionLevel
 from scarletbanner.wiki.models import Page
 
 
@@ -7,3 +8,9 @@ class PageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Page
         fields = ["id", "title", "slug", "body", "parent", "read", "write"]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["read"] = PermissionLevel(instance.read).name.replace("_", " ").title()
+        representation["write"] = PermissionLevel(instance.write).name.replace("_", " ").title()
+        return representation
